7d677b631ba7ad5e319c0255f3b3b1f54de9aa1b
eufrat <lukzyw@gmail.com>
2023-12-02 21:31:45 +0100

    tutorial: Ends game after winnner is clear

--- src/component/Board.js
+++ src/component/Board.js
@@ -3,16 +3,43 @@ import Square from "./Square";
 
 export default function Board() {
     const [player, setPlayer] = useState("X")
+    const [winner, setWinner] = useState(null)
     const [grid, setGrid] = useState([
         ["", "", ""],
         ["", "", ""],
         ["", "", ""]
     ])
 
+    const isWin = () => {
+        const lines = [
+            [[0 ,0], [0, 1], [0, 2]],
+            [[1 ,0], [1, 1], [1, 2]],
+            [[2 ,0], [2, 1], [2, 2]],
+            [[0, 0], [1, 0], [2, 0]],
+            [[0, 1], [1, 1], [2, 1]],
+            [[0, 2], [1, 2], [2, 2]],
+            [[0, 0], [1, 1], [2, 2]],
+            [[0, 2], [1, 1], [2, 0]]
+        ]
+        for (const line of lines) {
+            let winnerReq = 3
+            for (const position of line) {
+                if (grid[position[0]][position[1]] == player) {
+                    winnerReq--;
+                }
+            }
+            if (winnerReq==0) {
+                setWinner(player)
+            }
+        }
+    }
+
     const handleClick = (i, j) => {
-        if (grid[i][j] == "") {
-            grid[i][j] = player
-            setGrid(grid)
+        if (grid[i][j] == "" && winner == null) {
+            const newGrid = grid.slice()
+            newGrid[i][j] = player
+            setGrid(newGrid)
+            isWin()
             setPlayer(p => p=='X' ? 'O' : 'X')
         }
     }
